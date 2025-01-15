33e97b794a32891c8e11d73c7d4d2cb6b171e3d2
Benewinde KONTIEBO <e2405714@ensimlan.univ-lemans.fr>
2025-01-15 14:46:28 +0100

    page d'acueil

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vroomly</title>
+    <link rel="stylesheet" href="moncss.css">
+    <link rel="stylesheet" href="moncss1.css">
+</head>
+<body>
+    <div class="conteneur">
+        <div class="contenu">
+            <div class="container">
+                <nav>
+                    <img src="images/wanen_white.png" alt="" class="logo" />
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="">voitures</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                    <img src="images/cart.png" alt="" class="cart" />
+                </nav>
+
+                <section class="site-container">
+                    <p>Bienvenue sur</p>
+                    <h1>VROOMLY</h1>
+                    <h3>Locations de voitures</h3>
+
+                </section>
+
+                <section class="social-icons">
+                    <a href="#"><img src="images/github-fill.png" alt=""></a>
+                    <a href="#"><img src="images/instagram-fill.png" alt=""></a>
+                    <a href="#"><img src="images/telegram-fill.png" alt=""></a>
+                    <a href="https://drive.google.com/file/d/1u2ZQxyamKsgJ4JxO-kYbytfxaaEkOT_a/view?usp=sharing" target="_blank"><img src="images/drive-fill.png" alt=""></a>
+                </section>
+            </div>
+        </div>
+
+        <footer>
+            <div class="contenu-footer">
+                <div class="footer-services bloc">
+                    <h3>Nos services</h3>
+                    <ul>
+                        <li><a href="#">Locations de voitures</a></li>
+                        <li><a href="#">Ventes de voitures</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-contact bloc">
+                    <h3>Nous contacter</h3>
+                    <p>Téléphone : 55-55-55-55</p>
+                    <p>Email : supportclient@gmail.com</p>
+                    <p>Adresse : 6 rue de l'invention, Le Mans, 72100</p>
+                </div>
+
+                <div class="footer-horaires bloc">
+                    <h3>Nos horaires</h3>
+                    <p>Ouvert 7j/7</p>
+                    <p>24h/24</p>
+                </div>
+            </div>
+
+            <div class="pied-de-page">
+                <p>&copy; 2025 Vroomly. Tous droits réservés.</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
